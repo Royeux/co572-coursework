9432a19e553c7cf3d9061065522701ba4c49c08b
Nithya Krishnan <nithyaniti98@gmail.com>
2019-10-30 17:59:59 +0000

    astable query1

--- solution.c
+++ solution.c
@@ -4,7 +4,42 @@ int Query1(struct Database* db, int managerID, int price) {
   (void)db;        // prevent compiler warning about unused variable
   (void)managerID; // prevent compiler warning about unused variable
   (void)price;     // prevent compiler warning about unused variable
-  return 0;
+  
+  // Build refers to the qualifying Order and probe refers to the qualifying Items
+
+  struct HashTableSlot {
+    bool isOccupied;
+    struct OrderTuple value;
+  };
+  extern struct HashTableSlot* hashTable;
+
+  int bitsize = 100;
+  int hashSize = bitsize * 2;
+  for (size_t i=0; i < bitsize; i++) {
+    struct OrderTuple buildInput = build[i];
+    auto hashValue = hash(buildInput.salesDate, hashSize);
+    while (hashTable[hashValue].isOccupied) {
+      hashValue = nextSlot(hashValue, hashSize);
+    };
+    hashTable[hashValue].isOccupied = true;
+    hashTable[hashValue].value = buildInput;
+  };
+
+  int sortsize = 100;
+  int count = 0;
+  for (size_t i = 0; i < sortsize; i++) {
+    struct ItemTuple probeInput = probe[i];
+    auto hashValue = hash(probeInput.salesDate, hashSize);
+    while (hashTable[hashValue].isOccupied && hashTable[hashValue].value.salesDate != probeInput.salesDate) {
+      hashValue = nextSlot(hashValue, hashSize);
+    };
+    if(hashTable[hashValue].value.salesDate == probeInput.salesDate) {
+      if (hashTable[hashValue].value.employee == probeInput.employee) {
+        count += 1;
+      };
+    };
+  };
+  return count;
 }
 
 int Query2(struct Database* db, int discount, int date) {
@@ -28,3 +63,11 @@ void DestroyIndices(struct Database* db) {
   /// Free database indices
   db->indices = NULL;
 }
+
+int hash(int key, int hashSize) {
+  return key % (hashSize);
+};
+
+int nextSlot(int key, int hashSize) {
+  return (key + 1) % (hashSize);
+};
