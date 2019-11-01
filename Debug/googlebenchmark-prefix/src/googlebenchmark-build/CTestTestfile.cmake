b6921ddaecd57759ebf3074f3d45750e658f98ae
jeahwhay <maojw98@gmail.com>
2019-11-01 10:56:22 +0000

    removed release and debug

--- Debug/googlebenchmark-prefix/src/googlebenchmark-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/MuddyPie/Desktop/LIFE/REP/Y3/ICL/Autumn/CO572 Advanced Databases/co572-coursework/Debug/googlebenchmark-prefix/src/googlebenchmark
-# Build directory: /Users/MuddyPie/Desktop/LIFE/REP/Y3/ICL/Autumn/CO572 Advanced Databases/co572-coursework/Debug/googlebenchmark-prefix/src/googlebenchmark-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
